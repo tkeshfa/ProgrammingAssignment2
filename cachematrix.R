a81ecf6995bb7626536f1569cef8c5941b17f5d8
tkeshfa <farahaty@hotmail.com>
2015-08-20 21:13:17 -0400

    uploading the script of assingment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+setmatrix<-function(y){
+  x<<-y
+  m<<-NULL
+}
+getmatrix<-function() x
+setinvmatrix<-function(inversematrix) m<<-inversematrix
+getinvmatrix<- function() m
+list(setmatrix = setmatrix, getmatrix = getmatrix,
+     setinvmatrix = setinvmatrix,
+     getinvmatrix = getinvmatrix)
 }
 
 
@@ -12,4 +22,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m<-x$getinvmatrix()
+  if(!is.null(m)) {
+    message("getting the inverse matrix from cached data")
+    return(m)
+  }
+    
+  data<- x$getmatrix()
+  m<-solve(data,...)
+  x$setinvmatrix(m)
+  m
 }
+  
+
+  
+
+
+
+
